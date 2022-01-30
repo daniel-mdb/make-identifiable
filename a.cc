3ae74d1f521bce4eb5a840dfa76ea62010915a7f
Daniel Morilha <dmorilha@gmail.com>
2022-01-30 08:59:13 -0600

    Studied macros a little bit to understand how easy would be to inject a preamble into a callee
    Found some nice material, but unfortunately I am venturing uncharted
    waters ;-)
    
    https://www.boost.org/doc/libs/1_78_0/libs/preprocessor/doc/index.html
    http://jhnet.co.uk/articles/cpp_magic
    https://github.com/18sg/uSHET/blob/master/doc/ezshet_tutorial.md#appendix-h-peeking-behind-the-scenes

--- a.cc
+++ a.cc
@@ -2,12 +2,29 @@
 #include <functional>
 
 #pragma mark from here on
-void a() {
+#define identify(...) (__VA_ARGS__) { \
+  std::cout << "Function called " << __func__ << " (" << __FILE__ << ":" << __LINE__ << ")" << std::endl;
+
+void a identify(int a = 0) // {
   std::cout << "Hello World!" << std::endl;
 }
 
+#if 0
+/*
+ * the above macro would read nicer if the compiler could simply concatenate root
+ * level scopes like in:
+ * int b(void) { /* preamble */ } { return 0; }
+ */
+#endif
+
 int main() {
+
   a();
+
+  [] identify(int a = 1) // {
+    std::cout << "Good bye" << std::endl;
+  }();
+
   return 0;
 }
-#pragma mark to here
+#pragma mark until here
